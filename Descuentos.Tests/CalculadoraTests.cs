b1dc37bdfdd38183e74ad8330e821043fe3bd66e
immanuel448 <emmanuelmendoza448@gmail.com>
2025-07-31 15:04:47 -0600

    Recupera proyecto desde respaldo y corrige estructura

---
+++ Descuentos.Tests/CalculadoraTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Descuentos.Core;
+
+namespace Descuentos.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void DescuentoDel10_DeberiaDevolver90()
+        {
+            var calc = new Calculadora();
+            var resultado = calc.CalcularPrecioFinal(100, 10);
+            Assert.Equal(90, resultado);
+        }
+
+        [Fact]
+        public void DescuentoMayorA100_DeberiaLanzarExcepcion()
+        {
+            var calc = new Calculadora();
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                calc.CalcularPrecioFinal(100, 120));
+        }
+    }
+}
---
+++ Descuentos.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Descuentos.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
