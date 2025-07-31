57a469b0d167a6c87202d77c911a8b7d8c00d43d
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-07-30 22:42:12 -0600

    Agrega clase Calculadora y actualiza solución

---
+++ Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descuentos.Core
+{
+    public class Calculadora
+    {
+        public decimal CalcularPrecioFinal(decimal precioOriginal, int porcentajeDescuento)
+        {
+            if (porcentajeDescuento < 0 || porcentajeDescuento > 100)
+                throw new ArgumentOutOfRangeException(nameof(porcentajeDescuento));
+
+            return precioOriginal * (1 - (porcentajeDescuento / 100m));
+        }
+    }
+}
